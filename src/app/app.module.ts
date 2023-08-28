9c1b71beb97ebd2fad5983b75c13909495e7c95c
Marvel-Deone <141838290+Fola-Digit@users.noreply.github.com>
2023-08-28 21:46:02 +0100

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './public/menu/menu.component';
+import { HeaderComponent } from './public/header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
